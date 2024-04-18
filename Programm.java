4592f4dd014e10ac3504c8eb6850f62917bcb628
Lukas <136499316+mypictur3esforu@users.noreply.github.com>
2024-04-18 19:39:28 +0200

    Feld Finder
    Programm findet das Feld nun im Array

--- Programm.java
+++ Programm.java
@@ -5,14 +5,18 @@ public class Programm {
     String toMove = "white";
     String doneMove, move;
     String[]court = new String[64];
+    int squareNumber = 0;
 
     void start(){
-        CourtDefinition();
+        BordDefinition();
         EnterMove();
+        ConvertSquare();
+        CheckPiece();
+        CheckMove();
         ChangeTurn();
     }
     
-    void CourtDefinition(){
+    void BordDefinition(){
         int ersatzI = 0;
         String[] pieces = new String[]{"R","N","B","Q","K","B","N","R"};
         for (int i = 0; i < court.length; i++) {
@@ -31,7 +35,6 @@ public class Programm {
                 ersatzI++;
             }
         }
-        System.out.println(court);
 
     }
     void EnterMove(){
@@ -41,6 +44,47 @@ public class Programm {
         ScanObj.close();
         System.out.println(move);
     }
+
+    void ConvertSquare(){
+        String[] squareChosen = new String[10];
+        squareChosen = move.split("");
+        //move[1] = Column
+        //move[2] = Row
+        squareNumber = 63;
+        for (int i = 0; i < Integer.parseInt(squareChosen[2]); i++) {
+            squareNumber -= 8;
+        }
+        squareNumber += LetterConverter(squareChosen[1]);
+        System.out.println(squareNumber);
+    }
+    int LetterConverter(String letter){
+        int resolvingNumber = 0;
+        switch (letter){
+            case "a" -> resolvingNumber = 1;
+            case "b" -> resolvingNumber = 2;
+            case "c" -> resolvingNumber = 3;
+            case "d" -> resolvingNumber = 4;
+            case "e" -> resolvingNumber = 5;
+            case "f" -> resolvingNumber = 6;
+            case "g" -> resolvingNumber = 7;
+            case "h" -> resolvingNumber = 8;
+        }
+        return resolvingNumber;
+    }
+
+    void CheckPiece(){
+
+    }
+
+    void CheckMove(){
+
+    }
+    @SuppressWarnings("unused")
+    void MakeMove(){
+        String[] enteredMove = new String[10];
+        enteredMove = move.split("");
+    }
+
     void ChangeTurn(){
         if (toMove.equals("white") ) {
             toMove = "black";
@@ -48,15 +92,5 @@ public class Programm {
             toMove = "white";
         }
 
-    }
-    @SuppressWarnings("unused")
-    void MakeMove(){
-        String[] enteredMove = new String[10];
-        enteredMove = move.split("");
-        int fromRow = Integer.parseInt(enteredMove[0]);
-        int fromColumn = Integer.parseInt(enteredMove[1]);
-        int toRow = Integer.parseInt(enteredMove[2]);
-        int toColumn = Integer.parseInt(enteredMove[3]);
-        String chosenPiece = "";
     }
 }
